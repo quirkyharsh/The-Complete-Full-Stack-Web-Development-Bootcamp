7f69815f6ec9ddd527efc12a4e57f32fd0cfe88c
quirkyharsh <harshpatil3.1428@gmail.com>
2025-02-27 16:11:33 +0530

    complete drum kit projrct

--- JavaScript/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -5,44 +5,58 @@ for(var i = 0; i < 7; i++){
 
         var buttonINNERHTML = this.innerHTML;
 
-        switch (buttonINNERHTML) {
-            case "w":
-                var audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
+        makesound(buttonINNERHTML);
 
-            case "a":
-                var audio = new Audio("sounds/tom-2.mp3");
-                audio.play();
-            
-            case "s":
-                var audio = new Audio("sounds/tom-3.mp3");
-                audio.play();
-
-            case "d":
-                var audio = new Audio("sounds/tom-4.mp3");
-                audio.play();
-            
-            case "j":
-                var audio = new Audio("sounds/snare.mp3");
-                audio.play();
-            
-            case "k":
-                var audio = new Audio("sounds/crash.mp3");
-                audio.play();
-
-            case "l":
-                var audio = new Audio("sounds/kick-bass.mp3");
-                audio.play();
-
-            default:
-                break;
-        }
-
+        buttonAnimation(buttonINNERHTML);
     });
 
 }
 
+document.addEventListener("keypress", function (event){
+    makesound(event.key);
+    buttonAnimation(event.key);
+});
 
 
-// var audio = new Audio("sounds/tom-1.mp3");
-// audio.play();
+function makesound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+        
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+        
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+        
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(currentkey){
+    var howerBUTTON = document.querySelector("." + currentkey)
+    howerBUTTON.classList.add("pressed");
+    setTimeout(function(){
+        howerBUTTON.classList.remove("pressed",100);
+    });
+}
